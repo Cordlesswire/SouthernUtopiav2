c3d2ea63b8828e7ac83371ede72140dbae8c0703
Cordlesswire <chungwoo007@gmail.com>
2017-07-21 11:55:49 +0200

    Initial Commit

--- app/src/main/java/eu/laramartin/tokyoguide/Events.java
+++ app/src/main/java/eu/laramartin/tokyoguide/Events.java
@@ -11,71 +11,71 @@ public class Events {
     public static void initEventsList(List<Location> list, Context context) {
 
         list.add(new Location(
-                context.getString(R.string.shop_potato_name),
-                context.getString(R.string.shop_potato_description),
-                context.getString(R.string.shop_potato_address),
-                context.getString(R.string.shop_potato_phone),
-                context.getString(R.string.shop_potato_schedule),
+                context.getString(R.string.events_backtothecity_name),
+                context.getString(R.string.events_backtothecity_description),
+                context.getString(R.string.events_backtothecity_address),
+                context.getString(R.string.events_backtothecity_phone),
+                context.getString(R.string.events_backtothecity_schedule),
                 null,
                 R.drawable.events_backtothecity
         ));
 
         list.add(new Location(
-                context.getString(R.string.shop_amiami_name),
-                context.getString(R.string.shop_amiami_description),
-                context.getString(R.string.shop_amiami_address),
-                context.getString(R.string.shop_amiami_phone),
-                context.getString(R.string.shop_amiami_schedule),
+                context.getString(R.string.events_dinnerinthesky_name),
+                context.getString(R.string.events_dinnerinthesky_description),
+                context.getString(R.string.events_dinnerinthesky_address),
+                context.getString(R.string.events_dinnerinthesky_phone),
+                context.getString(R.string.events_dinnerinthesky_schedule),
                 null,
                 R.drawable.events_dinnerinthesky
         ));
 
         list.add(new Location(
-                context.getString(R.string.shop_nakano_name),
-                context.getString(R.string.shop_nakano_description),
-                context.getString(R.string.shop_nakano_address),
-                context.getString(R.string.shop_nakano_phone),
-                context.getString(R.string.shop_nakano_schedule),
+                context.getString(R.string.events_jozibookfair_name),
+                context.getString(R.string.events_jozibookfair_description),
+                context.getString(R.string.events_jozibookfair_address),
+                context.getString(R.string.events_jozibookfair_phone),
+                context.getString(R.string.events_jozibookfair_schedule),
                 null,
                 R.drawable.events_jozibookfair
         ));
 
         list.add(new Location(
-                context.getString(R.string.shop_kiddy_name),
-                context.getString(R.string.shop_kiddy_description),
-                context.getString(R.string.shop_kiddy_address),
-                context.getString(R.string.shop_kiddy_phone),
-                context.getString(R.string.shop_kiddy_schedule),
+                context.getString(R.string.events_outdoorscreening_name),
+                context.getString(R.string.events_outdoorscreening_description),
+                context.getString(R.string.events_outdoorscreening_address),
+                context.getString(R.string.events_outdoorscreening_phone),
+                context.getString(R.string.events_outdoorscreening_schedule),
                 null,
                 R.drawable.events_outdoorscreening
         ));
 
         list.add(new Location(
-                context.getString(R.string.shop_daiso_name),
-                context.getString(R.string.shop_daiso_description),
-                context.getString(R.string.shop_daiso_address),
-                context.getString(R.string.shop_daiso_phone),
-                context.getString(R.string.shop_daiso_schedule),
+                context.getString(R.string.events_sowetotheatre_name),
+                context.getString(R.string.events_sowetotheatre_description),
+                context.getString(R.string.events_sowetotheatre_address),
+                context.getString(R.string.events_sowetotheatre_phone),
+                context.getString(R.string.events_sowetotheatre_schedule),
                 null,
                 R.drawable.events_sowetotheatre
         ));
 
         list.add(new Location(
-                context.getString(R.string.shop_mandarake_name),
-                context.getString(R.string.shop_mandarake_description),
-                context.getString(R.string.shop_mandarake_address),
-                context.getString(R.string.shop_mandarake_phone),
-                context.getString(R.string.shop_mandarake_schedule),
+                context.getString(R.string.events_strcrd_name),
+                context.getString(R.string.events_strcrd_description),
+                context.getString(R.string.events_strcrd_address),
+                context.getString(R.string.events_strcrd_phone),
+                context.getString(R.string.events_strcrd_schedule),
                 null,
                 R.drawable.events_strcrd
         ));
 
         list.add(new Location(
-                context.getString(R.string.shop_kinokuniya_name),
-                context.getString(R.string.shop_kinokuniya_description),
-                context.getString(R.string.shop_kinokuniya_address),
-                context.getString(R.string.shop_kinokuniya_phone),
-                context.getString(R.string.shop_kinokuniya_schedule),
+                context.getString(R.string.events_vsgaming_name),
+                context.getString(R.string.events_vsgaming_description),
+                context.getString(R.string.events_vsgaming_address),
+                context.getString(R.string.events_vsgaming_phone),
+                context.getString(R.string.events_vsgaming_schedule),
                 null,
                 R.drawable.events_vsgaming
         ));
