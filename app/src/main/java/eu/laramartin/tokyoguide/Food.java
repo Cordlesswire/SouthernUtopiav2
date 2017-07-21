762844ba3ad7ace9ac2c7dd95c814c228ffbae7c
Cordlesswire <chungwoo007@gmail.com>
2017-07-21 12:36:48 +0200

    Initial Commit

--- app/src/main/java/eu/laramartin/tokyoguide/Food.java
+++ app/src/main/java/eu/laramartin/tokyoguide/Food.java
@@ -9,73 +9,73 @@ public class Food {
     public static void initFoodsList(List<Location> list, Context context) {
 
         list.add(new Location(
-                context.getString(R.string.food_genki_name),
-                context.getString(R.string.food_genki_description),
-                context.getString(R.string.food_genki_address),
-                context.getString(R.string.food_genki_phone),
-                context.getString(R.string.food_genki_schedule),
-                context.getString(R.string.food_price_two),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_chafpozi_name),
+                context.getString(R.string.food_chafpozi_description),
+                context.getString(R.string.food_chafpozi_address),
+                context.getString(R.string.food_chafpozi_phone),
+                context.getString(R.string.food_chafpozi_schedule),
+                context.getString(R.string.food_chafpozi_price),
+                R.drawable.food_chafpozi
         ));
 
         list.add(new Location(
-                context.getString(R.string.food_wako_name),
-                context.getString(R.string.food_wako_description),
-                context.getString(R.string.food_wako_address),
-                context.getString(R.string.food_wako_phone),
-                context.getString(R.string.food_wako_schedule),
-                context.getString(R.string.food_price_two),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_licorish_name),
+                context.getString(R.string.food_licorish_description),
+                context.getString(R.string.food_licorish_address),
+                context.getString(R.string.food_licorish_phone),
+                context.getString(R.string.food_licorish_schedule),
+                context.getString(R.string.food_licorish_price),
+                R.drawable.food_licorish
         ));
 
         list.add(new Location(
-                context.getString(R.string.food_rokumonya_name),
-                context.getString(R.string.food_rokumonya_description),
-                context.getString(R.string.food_rokumonya_address),
-                context.getString(R.string.food_rokumonya_phone),
-                context.getString(R.string.food_rokumonya_schedule),
-                context.getString(R.string.food_price_two),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_luckybean_name),
+                context.getString(R.string.food_luckybean_description),
+                context.getString(R.string.food_luckybean_address),
+                context.getString(R.string.food_luckybean_phone),
+                context.getString(R.string.food_luckybean_schedule),
+                context.getString(R.string.food_luckybean_price),
+                R.drawable.food_luckybean
         ));
 
         list.add(new Location(
-                context.getString(R.string.food_kisoji_name),
-                context.getString(R.string.food_kisoji_description),
-                context.getString(R.string.food_kisoji_address),
-                context.getString(R.string.food_kisoji_phone),
-                context.getString(R.string.food_kisoji_schedule),
-                context.getString(R.string.food_price_four),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_marble_name),
+                context.getString(R.string.food_marble_description),
+                context.getString(R.string.food_marble_address),
+                context.getString(R.string.food_marble_phone),
+                context.getString(R.string.food_marble_schedule),
+                context.getString(R.string.food_marble_price),
+                R.drawable.food_marble
         ));
 
         list.add(new Location(
-                context.getString(R.string.food_gogo_name),
-                context.getString(R.string.food_gogo_description),
-                context.getString(R.string.food_gogo_address),
-                context.getString(R.string.food_gogo_phone),
-                context.getString(R.string.food_gogo_schedule),
-                context.getString(R.string.food_price_one),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_patapata_name),
+                context.getString(R.string.food_patapata_description),
+                context.getString(R.string.food_patapata_address),
+                context.getString(R.string.food_patapata_phone),
+                context.getString(R.string.food_patapata_schedule),
+                context.getString(R.string.food_patapata_price),
+                R.drawable.food_patapata
         ));
 
         list.add(new Location(
-                context.getString(R.string.food_tenya_name),
-                context.getString(R.string.food_tenya_description),
-                context.getString(R.string.food_tenya_address),
-                context.getString(R.string.food_tenya_phone),
-                context.getString(R.string.food_tenya_schedule),
-                context.getString(R.string.food_price_one),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_thehill_name),
+                context.getString(R.string.food_thehill_description),
+                context.getString(R.string.food_thehill_address),
+                context.getString(R.string.food_thehill_phone),
+                context.getString(R.string.food_thehill_schedule),
+                context.getString(R.string.food_thehill_price),
+                R.drawable.food_thehill
         ));
 
         list.add(new Location(
-                context.getString(R.string.food_shibukatsu_name),
-                context.getString(R.string.food_shibukatsu_description),
-                context.getString(R.string.food_shibukatsu_address),
-                context.getString(R.string.food_shibukatsu_phone),
-                context.getString(R.string.food_shibukatsu_schedule),
-                context.getString(R.string.food_price_one),
-                R.drawable.sight_cradle_of_humankind
+                context.getString(R.string.food_vuyosbraai_name),
+                context.getString(R.string.food_vuyosbraai_description),
+                context.getString(R.string.food_vuyosbraai_address),
+                context.getString(R.string.food_vuyosbraai_phone),
+                context.getString(R.string.food_vuyosbraai_schedule),
+                context.getString(R.string.food_vuyosbraai_price),
+                R.drawable.food_vuyosbraai
         ));
     }
 }
